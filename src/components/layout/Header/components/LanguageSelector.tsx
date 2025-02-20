1ef709ea080c27309fd8a31f02ffbadb82a57b36
BekaChkhiro <bekachkhirodze1@gmail.com>
2025-02-20 11:52:54 +0400

    language switcher changed

--- src/components/layout/Header/components/LanguageSelector.tsx
+++ src/components/layout/Header/components/LanguageSelector.tsx
@@ -25,7 +25,7 @@ const LanguageSelector = ({ currentLanguage, languages, onLanguageChange }: Lang
       
       <div className="invisible group-hover:visible opacity-0 group-hover:opacity-100 
         absolute top-full right-0 mt-1 bg-white rounded-lg shadow-lg 
-        border border-gray-100 py-2 w-40 transition-all duration-300
+        border border-gray-100 py-2 w-23 transition-all duration-300
         transform translate-y-2 group-hover:translate-y-0">
         {languages.filter(lang => lang.name !== currentLanguage).map(lang => (
           <button 
